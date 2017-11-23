7f228b3216375ec552c1d54e9c7a3ea2470902f9
yokrh <y.oka.gml@gmail.com>
2017-11-23 13:07:43 +0000

    change links

--- src/component/links.vue
+++ src/component/links.vue
@@ -2,16 +2,22 @@
   <dl class="links">
     <dt class="title">リンク集</dt>
     <dd class="link">
-      <a href="https://p.eagate.573.jp/game/sdvx/">SDVX IV公式</a>
+      <a href="https://p.eagate.573.jp/game/sdvx/">SOUND VOLTEX IV HEAVENLY HAVEN</a>
     </dd>
     <dd class="link">
-      <a href="http://bemaniwiki.com/index.php?SOUND%20VOLTEX%20IV%20HEAVENLY%20HAVEN">SDVX Wiki</a>
+      <a href="http://bemaniwiki.com/index.php?SOUND%20VOLTEX%20IV%20HEAVENLY%20HAVEN">SDVX IV - BEMANIWiki 2nd</a>
     </dd>
     <dd class="link">
-      <a href="http://nearnoah.sakura.ne.jp/sdvxiv/">スコアツール</a>
+      <a href="http://nearnoah.sakura.ne.jp/sdvxiv/">SDVX IV スコアツール（仮）</a>
     </dd>
     <dd class="link">
       <a href="http://www.sdvx.in/">SDVX譜面保管所IV</a><span class="special">★ Ultimate Special Thanks</span>
+    </dd>
+    <dd class="link">
+      <a href="https://p.eagate.573.jp/game/bemani/hinabita/p/bittersweets/bio/index.html">日向美ビタースイーツ♪ | ひなビタ♪</a>
+    </dd>
+    <dd class="link">
+      <a href="https://hbfb.herokuapp.com/">ひなビタ♪BIOGRAPHY検索（β）</a>
     </dd>
   </dl>
 </template>
