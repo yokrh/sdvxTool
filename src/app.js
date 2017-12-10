3ba1e2a2c5823f8056cd984a0e67e7c49ed9a60c
yokrh <y.oka.gml@gmail.com>
2017-12-10 10:42:17 +0000

    change import element ui on demand

--- src/app.js
+++ src/app.js
@@ -7,9 +7,9 @@ import './image/icon-quiz.png';
 
 import Vue from 'Vue'
 import VueRouter from 'vue-router'
-import ElementUI from 'element-ui'
-import locale from '../node_modules/element-ui/lib/locale/lang/ja'
-import '../node_modules/element-ui/lib/theme-chalk/index.css';
+import locale from '../node_modules/element-ui/lib/locale'
+import lang from '../node_modules/element-ui/lib/locale/lang/ja'
+import '../node_modules/element-ui/lib/theme-chalk/index.css'
 
 import appHeader from './component/appHeader.vue'
 import appContent from './component/appContent.vue'
@@ -17,8 +17,8 @@ import trackSearch from './component/trackSearch.vue'
 import weekly from './component/weekly.vue'
 import links from './component/links.vue'
 
+locale.use(lang);
 Vue.use(VueRouter);
-Vue.use(ElementUI, {locale});
 
 const routes = [
   { path: '/', component: trackSearch },
