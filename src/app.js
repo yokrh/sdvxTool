da02cde6381502fe51ced4a522fbc6c1c0258a6d
yokrh <y.oka.gml@gmail.com>
2018-01-04 00:46:12 +0000

    add img icon plus

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import './image/icon-link.png';
 import './image/icon-twitter.png';
 import './image/icon-weekly.png';
 import './image/icon-quiz.png';
+import './image/icon-plus.png';
 
 import Vue from 'Vue'
 import VueRouter from 'vue-router'
