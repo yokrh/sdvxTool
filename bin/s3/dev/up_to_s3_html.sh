f3e98ab48fb0d73caf13880a56d5bfe94e21e153
yokrh <y.oka.gml@gmail.com>
2018-03-03 04:00:04 +0000

    delete bin unused

--- bin/s3/dev/up_to_s3_html.sh
+++
@@ -1,1 +0,0 @@
-aws s3 cp ../../../dist/index.html s3://sdvxdev/index.html --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
--- bin/s3/prod/up_to_s3_css.sh
+++
@@ -1,1 +0,0 @@
-aws s3 cp ../../../dist/css/app.bundle.css s3://sdvx/css/app.bundle.css
