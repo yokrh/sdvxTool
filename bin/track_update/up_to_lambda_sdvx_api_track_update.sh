255e3a66ac2eb1c4bd960cbd11fcae1d563b3123
yokrh <y.oka.gml@gmail.com>
2018-04-08 03:14:42 +0000

    Revert "try to run puppetter on aws lambda, but no shared library error"
    This reverts commit f6f625ef07b6cff12d6560fbc475aaf4e0e02f0a.

--- bin/track_update/up_to_lambda_sdvx_api_track_update.sh
+++ bin/track_update/up_to_lambda_sdvx_api_track_update.sh
@@ -1,11 +1,9 @@
-# rm -rf ~/.cache/pip/
-# rm -rf ~/.local/lib/python2.7/site-packages/awscli-1.15.1.dist-info/
-# sudo apt-get install awscli
+# sudo apt-get -install awscli
 # sudo pip install --upgrade awscli
 # aws --version
 # mkdir lambda
-### delete some files to resolve 'Unzipped size must be smaller than 262144000 bytes'
+# pip install BeautifulSoup4 -t lambda_function
+## pip install --upgrade --force-reinstall BeautifulSoup4
 cd ./lambda
-rm lambda-sdvx-api-track-update.zip
 zip -r lambda-sdvx-api-track-update.zip *
 aws lambda --region ap-northeast-1 update-function-code --function-name sdvx-api-track-update --zip-file fileb://lambda-sdvx-api-track-update.zip
